53d80020d4ffa77e82f7d170aef7cfd9ffea0a71
LevX <team@levx.io>
2022-10-24 20:33:15 +0900

    Force swaps in FeeVault.claimFees()

--- contracts/FeeVault.sol
+++ contracts/FeeVault.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.15;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/IVotingEscrow.sol";
 import "./interfaces/IFeeVault.sol";
+import "./interfaces/IUniswapV2Router02.sol";
 import "./libraries/Tokens.sol";
 import "./libraries/Integers.sol";
 
@@ -17,13 +18,22 @@ contract FeeVault is IFeeVault {
 
     address public immutable override weth;
     address public immutable override votingEscrow;
+    address public immutable override rewardToken;
+    address public immutable override swapRouter;
     mapping(address => uint256) public override balances;
     mapping(address => Fee[]) public override fees;
     mapping(address => mapping(address => uint256)) public override lastFeeClaimed;
 
-    constructor(address _weth, address _votingEscrow) {
+    constructor(
+        address _weth,
+        address _votingEscrow,
+        address _rewardToken,
+        address _swapRouter
+    ) {
         weth = _weth;
         votingEscrow = _votingEscrow;
+        rewardToken = _rewardToken;
+        swapRouter = _swapRouter;
     }
 
     receive() external payable {
@@ -57,9 +67,20 @@ contract FeeVault is IFeeVault {
      * @notice Claim accumulated fees
      * @param token In which currency fees were paid
      * @param to the last index of the fee (exclusive)
+     * @param amountRewardMin Minimum amount of reward after swapping
+     * @param path Route for swapping fees
+     * @param deadline Expiration timestamp
      */
-    function claimFees(address token, uint256 to) external override {
+    function claimFees(
+        address token,
+        uint256 to,
+        uint256 amountRewardMin,
+        address[] calldata path,
+        uint256 deadline
+    ) external override {
         require(to < fees[token].length, "FV: INDEX_OUT_OF_RANGE");
+        require(path[0] == (token == address(0) ? weth : token), "FV: INVALID_PATH");
+        require(path[path.length - 1] == rewardToken, "FV: INVALID_PATH");
 
         uint256 from = lastFeeClaimed[token][msg.sender];
 
@@ -79,7 +100,26 @@ contract FeeVault is IFeeVault {
         }
         lastFeeClaimed[token][msg.sender] = to;
 
-        emit ClaimFees(token, amount, msg.sender);
-        Tokens.safeTransfer(token, msg.sender, amount, weth);
+        if (amount > 0) {
+            if (token == address(0)) {
+                uint256[] memory amounts = IUniswapV2Router02(swapRouter).swapExactETHForTokens{value: amount}(
+                    amountRewardMin,
+                    path,
+                    msg.sender,
+                    deadline
+                );
+                emit ClaimFees(token, amount, amounts[amounts.length - 1], msg.sender);
+            } else {
+                IERC20(token).approve(swapRouter, amount);
+                uint256[] memory amounts = IUniswapV2Router02(swapRouter).swapExactTokensForTokens(
+                    amount,
+                    amountRewardMin,
+                    path,
+                    msg.sender,
+                    deadline
+                );
+                emit ClaimFees(token, amount, amounts[amounts.length - 1], msg.sender);
+            }
+        }
     }
 }
--- contracts/interfaces/IFeeVault.sol
+++ contracts/interfaces/IFeeVault.sol
@@ -3,11 +3,15 @@ pragma solidity ^0.8.0;
 
 interface IFeeVault {
     event Checkpoint(address indexed token, uint256 id, uint256 amount);
-    event ClaimFees(address indexed token, uint256 amount, address indexed to);
+    event ClaimFees(address indexed token, uint256 amount, uint256 amountReward, address indexed to);
 
     function weth() external view returns (address);
 
     function votingEscrow() external view returns (address);
+
+    function rewardToken() external view returns (address);
+
+    function swapRouter() external view returns (address);
 
     function balances(address token) external view returns (uint256);
 
@@ -19,5 +23,11 @@ interface IFeeVault {
 
     function checkpoint(address token) external;
 
-    function claimFees(address token, uint256 to) external;
+    function claimFees(
+        address token,
+        uint256 to,
+        uint256 amountRewardMin,
+        address[] calldata path,
+        uint256 deadline
+    ) external;
 }
